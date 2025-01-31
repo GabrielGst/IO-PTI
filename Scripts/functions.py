04e2dee08c669bcbe33e9ca44b28b644177a29bb
GabrielGst <gabriel@webagab.xyz>
2025-01-31 16:20:59 +0100

    Wrapped up main and modules

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -12,90 +12,89 @@ import numpy as np
 import scipy as sp
 
 def createSyntheticImage(oObject, psf_1, psf_2, alpha_0, verbose=False):
+    """Create the synthetic image using the object, the two PSFs and the alpha value. The PSF is a linear combination of the two PSFs. The simulated image is computed using the object and the PSF. The noise is added to the simulated image. The noised image is returned.
+
+    Args:
+        oObject (float): Object derived from the dsp of the object.
+        psf_1 (float): First PSF imported.
+        psf_2 (float): Second PSF imported.
+        alpha_0 (float): Between 0 and 1, the weight of the first PSF in the final PSF.
+        verbose (bool, optional): Show and save the generated images (choice defined within the function). Defaults to False.
+
+    Returns:
+        tuple: (float, float) The noised image and the standard deviation of the noise.
+    """
+    
     # Define the synthetic image
     ## Define PSF
     psf = alpha_0 * psf_1 + (1 - alpha_0) * psf_2 # The final PSF is a linear combination of the two PSFs
-
-    psfPadded = np.pad(psf, (768, 768), 'constant') # Use the padded psf for the Fourier transform
-    
+    psfPadded = np.pad(psf, (768, 768), 'constant') # Object is of size 1024x1024, derived from a dsp of size 2048x2048
     
     ### Plot psf for quality check
     if verbose:
-        saveImplot(psfPadded, f"Resulting psf (padded) for alpha = {alpha_0}", filename=f"Resulting psf (padded) for alpha = {alpha_0}", save=True, plot=True)
+        saveImplot(psfPadded, f"PSF of the synthetic image for alpha = {alpha_0}", filename=f"PSF of the synthetic image for alpha = {alpha_0}", save=True, plot=False)
 
+
+    ## Shift and crop the PSF to corresponding object size and coordinates reference
     psfPadded = sp.fft.ifftshift(psfPadded)
     psfPadded = psfPadded[:1024, :1024]
-        
-        
-    ### Plot psf for quality check
-    if verbose:
-        saveImplot(psfPadded, f"Resulting psf (padded + shift) for alpha = {alpha_0}", filename=f"Resulting psf (padded + shift) for alpha = {alpha_0}", save=True, plot=True)
-
-    tfPsf = sp.fft.fft2(psfPadded)
+    
+    
+    # ## Compute the Fourier transform of the PSF
+    # tfPsf = sp.fft.fft2(psfPadded) 
      
-     
-    ### Plot psf for quality check
-    if verbose:
-        saveImplot(np.abs(tfPsf), f"TF of the PSF for alpha = {alpha_0}", filename=f"TF of PSF for alpha = {alpha_0}", save=True, plot=True)
+    # ### Plot psf for quality check
+    # if verbose:
+    #     saveImplot(np.abs(tfPsf), f"TF of the PSF for alpha = {alpha_0}", filename=f"TF of PSF for alpha = {alpha_0}", save=True, plot=True)
 
-    simImage = sp.fft.ifft2(sp.fft.fft2(oObject) * sp.fft.fft2(psfPadded))# Q: Should the psf be fftshifted here ?
+
+    ## Compute the simulated image
+    simImage = sp.fft.ifft2(sp.fft.fft2(oObject) * sp.fft.fft2(psfPadded)) 
     sigma = 0.01 * np.max(oObject)
     noise = sigma * np.random.randn(*simImage.shape) # Noise corresponding roughly to photon noise for an average of 10 000 photons/pixels
     simImageNoised = simImage + noise
     
-
     ### Plot simulated object, noise, image for quality check
     if verbose:
         saveImsubplots([oObject, simImage.real, noise, simImageNoised.real], ["Object", "Image", "Noise", "Noised Image"], filename="simulated_object_image_noisedImage", save=True, plot=True)
 
 
-    
     return simImageNoised, sigma
-
-
 
 def jointMAP2psf(oObject, dspObject, psf_1, psf_2, alpha, simImageNoised, sigma):
     """Performs the joint estimation of the object and the PSF using the Jmap criterion. The criterion is computed for the given object, the two PSFs and the alpha value. The simulated image is computed using the object and the PSF. The noise is added to the simulated image. The noise and signal DSPs are computed. The Jmap criterion is computed and returned.
 
     Args:
-        oObject (float): Array representing the object to be estimated.
-        psf_1 (float): Array representing the first PSF.
-        psf_2 (float): Array representing the second PSF.
-        alpha (float): Scalar representing the weight of the first PSF in the final PSF.
-        verbose (bool, optional): Display and save intermediate plots. Defaults to False.
+        oObject (float): Object derived from the dsp of the object.
+        dspObject (float): DSP of the object.
+        psf_1 (float): First PSF imported.
+        psf_2 (float): Second PSF imported.
+        alpha (float): Between 0 and 1, the tested weight of the first PSF in the final PSF.
+        simImageNoised (float): Synthetic image for which the value of alpha is searched.
+        sigma (float): noise standard deviation.
 
     Returns:
-        float: Jmap criterion value. The joint MAP solution is the value of alpha that minimize Jmap.
+        float: Jmap criterion value for the selected alpha.
     """
+       
+    ## Tested alpha and PSF
+    h = alpha * psf_1 + (1 - alpha) * psf_2 # The final PSF is a linear combination of the two PSFs
+    hPad = np.pad(h, (768, 768), 'constant') # Object is of size 1024x1024, derived from a dsp of size 2048x2048
     
-    ## Joint estimation
-
-    h = alpha * psf_1 + (1 - alpha) * psf_2
     
-    hPad = np.pad(h, (768, 768), 'constant') # Use the padded psf for the Fourier transform
+    ## Shift and crop the PSF to corresponding object size and coordinates reference
     hPad = sp.fft.ifftshift(hPad)
     hPad = hPad[:1024, :1024]
-        
 
 
     ## Compute the noise and signal DSPs
-    # dspNoise = np.abs(sp.fft.ifft2(sp.fft.fft2(noise) * sp.fft.fft2(noise).conj()))**2 # Correlation therorem
-    # dspObject = sp.fft.ifft2(sp.fft.fft2(oObject) * sp.fft.fft2(oObject).conj()).real # Correlation therorem
     dspObject = dspObject[:1024,:1024] # The DSP of the object is known
-    
-
-    ### Plot simulated object, noise, image for quality check
-    # if verbose:
-    #     saveImsubplots([dspNoise, dspObject], ["DSP of the synthetic noise", "DSP of the synthetic object"], filename="dsp_noise_and_object", columnNumber=2, save=True, plot=True, logScale=True)
-
-
-    ## Compute the Jjmap criterion and plot it
     N =  simImageNoised.shape[0] * simImageNoised.shape[1] # Number of pixels
     dspNoise = sigma**2 * N # dspNoise.mean() # Average noise power, the noise is white so it is constant, one could have taken dspNoise[0,0] = sigma^2 ici attention à la normalisation avec le nombre de pixel pour garder l'énergie constante
     meanObject = oObject.mean() * np.ones(oObject.shape)
-    # meanObject = np.zeros(oObject.shape)
 
 
+    ## Compute the Jmap criterion
     Jmap = (
         0.5 * N * np.log(dspNoise) + # Here we removed the N**2 factor because it led to a very large negative offset (1e12 order of magnitude). I think it has already been taken into account in the definition of the dsp    
         0.5 * np.sum(np.log(dspObject)) +
@@ -108,27 +107,62 @@ def jointMAP2psf(oObject, dspObject, psf_1, psf_2, alpha, simImageNoised, sigma)
     
     return Jmap, dspNoise, hPad, N
 
-
 def marginalML2psf(oObject, dspObject, psf_1, psf_2, alpha, simImageNoised, sigma,):
-    """Performs the joint estimation of the object and the PSF using the Jmap criterion. The criterion is computed for the given object, the two PSFs and the alpha value. The simulated image is computed using the object and the PSF. The noise is added to the simulated image. The noise and signal DSPs are computed. The Jmap criterion is computed and returned.
+    """Performs the marginal estimation of the object and the PSF using the Jmap criterion. The criterion is computed for the given object, the two PSFs and the alpha value. The simulated image is computed using the object and the PSF. The noise is added to the simulated image. The noise and signal DSPs are computed. The Jmap criterion is computed and returned.
 
     Args:
-        oObject (float): Array representing the object to be estimated.
-        psf_1 (float): Array representing the first PSF.
-        psf_2 (float): Array representing the second PSF.
-        alpha (float): Scalar representing the weight of the first PSF in the final PSF.
-        verbose (bool, optional): Display and save intermediate plots. Defaults to False.
+        oObject (float): Object derived from the dsp of the object.
+        dspObject (float): DSP of the object.
+        psf_1 (float): First PSF imported.
+        psf_2 (float): Second PSF imported.
+        alpha (float): Between 0 and 1, the tested weight of the first PSF in the final PSF.
+        simImageNoised (float): Synthetic image for which the value of alpha is searched.
+        sigma (float): noise standard deviation.
 
     Returns:
-        float: Jmap criterion value. The joint MAP solution is the value of alpha that minimize Jmap.
+        (float, float, int): (Jmap, dspNoise, N) Jmap criterion value for the selected alpha, noise DSP and number of pixels.
     """
+    
+    ## Retrieve Jmap criterion and other parameters
     Jmap, dspNoise, hPad, N = jointMAP2psf(oObject, dspObject, psf_1, psf_2, alpha, simImageNoised, sigma)
+    
+    
+    ## Crop the dspObject to the same size as the object
     dspObject = dspObject[:1024,:1024]
 
+
+    ## Compute the Jml criterion
     Jml = (
         Jmap +
         0.5 * np.sum(np.log(np.abs(sp.fft.fft2(hPad))**2 + dspNoise / dspObject)) - 
         0.5 * N * np.log(dspNoise) # Here we removed the N**2 factor because it led to a very large negative offset (1e12 order of magnitude). I think it has already been taken into account in the definition of the dsp
     )
     
-    return Jml
+    return Jml, dspNoise, N
+
+def estimatedImage(simImageNoised, oObject, dspObject, dspNoise, alpha, psf_1, psf_2):
+    """Reconstruct an estimated image based on the alpha resulting from the Marginal estimation. The estimated image is computed using the object, the simulated image, the noise and the two PSFs. The final PSF is a linear combination of the two PSFs.
+
+    Args:
+        simImageNoised (float): Synthetic image for which the value of alpha is searched.
+        oObject (float): Object derived from the dsp of the object.
+        dspObject (float): DSP of the object.
+        dspNoise (float): DSP of the noise.
+        alpha (float): Estimated value of alpha.
+        psf_1 (float): First PSF imported.
+        psf_2 (float): Second PSF imported.
+    """
+    
+    ## Define the estimated PSF and average of the object
+    psf = alpha * psf_1 + (1 - alpha) * psf_2 # The final PSF is a linear combination of the two PSFs
+    oM = np.mean(oObject) * np.ones(oObject.shape)
+    
+    ## Compute the estimated image
+    im = (
+        (sp.fft.fft2(psf.conj()) * sp.fft.fft2(simImageNoised) + dspNoise / dspObject * oM) /
+         (np.abs(sp.fft.fft2(psf))**2 + dspNoise / dspObject)
+    )
+    
+    ### Plot psf for quality check
+    saveImplot(im, f"Estimated object for estimated alpha = {alpha}", filename=f"Estimated object for estimated alpha = {alpha}", save=True, plot=True)
+
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,135 +1,137 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
+from astropy.io import fits # Loading PSF with astropy module, that offers a loading function for Flexible Image Transport Image
+from tqdm.auto import tqdm
+
 
 from plotLib import saveImplot, saveImsubplots, savePlot
 
 # Define the path to save figures in plotLib SAVE_PATH = 'IO-PTI/outputs/' # Q: Why do we need to use the absolute path ?
 
+def createObject(nPoints = 2048, sampling_rate = 1, p=2, k=1, rho_0=0.01):
+    # Define the gaussian field in polar coordinates (freq.)
+    ## Define the polar grid
+    freq_vector = sp.fft.fftfreq(nPoints, d=1/sampling_rate) # Frequency vector for FFT, Numpy array
+    xFreq_array, yFreq_array = np.meshgrid(freq_vector, freq_vector) # Generating 2D array of frequencies from freq_vector
+
+
+    ## Convert Cartesian (X, Y) to Polar (r, theta)
+    rho = np.hypot(xFreq_array, yFreq_array)  # Radial distance
+    theta = np.arctan2(xFreq_array, yFreq_array)  # Angle in radians
+
+
+    ### Plot modulus and phase for quality check
+    saveImplot(rho, "Polar modulus", filename="polar_modulus", save=True, plot=False)
+    saveImplot(theta, "Polar phase", filename="polar_phase", save=True, plot=False)
+
+
+    ## Computes the dsp of the object
+    dspObject = k /(1 + pow(rho/rho_0,p))
+    profile = dspObject[0,:1024] # Profile of the radial distance
+    freqProfile = freq_vector[:1024] # Profile of the frequency vector
+
+
+    ### Plot dspObject for quality check
+    savePlot(freqProfile, profile, "Profile of the radial distance", filename="profile_radial_distance", save=True, plot=False, logX=True, logY=True)
+    saveImplot(dspObject, f"dspObject for rho_0 = {rho_0}", filename=f"dspObject for rho_0 = {rho_0}", save=True, plot=False, logScale=True)
+
+
+    ## Generate the random variable
+    tfObject = np.sqrt(dspObject) * np.random.randn(nPoints, nPoints) # Equivalently : np.sqrt(dspObject) * np.random.randn(nPoints, nPoints)
 
 
 
-
-# Define the gaussian field in polar coordinates (freq.)
-## Define the polar grid
-nPoints = 2048 # Number of points
-sampling_rate = 1  # Sampling rate
-freq_vector = sp.fft.fftfreq(nPoints, d=1/sampling_rate) # Frequency vector for FFT, Numpy array
-xFreq_array, yFreq_array = np.meshgrid(freq_vector, freq_vector) # Generating 2D array of frequencies from freq_vector
+    ## Derive the object
+    oObject = np.real(sp.fft.ifft2(tfObject))
+    oShapes = oObject.shape
+    oObject = oObject[:int(oShapes[0]/2),:int(oShapes[1]/2)] # Select first quadrant, int is equivalent to floor
+    oObject = oObject - np.min(oObject) # Remove the offset
 
 
-## Convert Cartesian (X, Y) to Polar (r, theta)
-rho = np.hypot(xFreq_array, yFreq_array)  # Radial distance
-theta = np.arctan2(xFreq_array, yFreq_array)  # Angle in radians
+    ### Plot oObject for quality check
+    saveImplot(oObject, f"Object for rho_0 = {rho_0}", filename=f"Object for rho_0 = {rho_0}", save=True, plot=False, logScale=False)
+
+    return oObject, dspObject
+
+def openPsfs(system: str):
+    if system == "windows":
+        filename = 'IO-PTI/data/3psfs_zeroPiSur2EtPi.fits' # Open the FITS file on windows
+    elif system == "ubuntu":
+        filename = './data/3psfs_zeroPiSur2EtPi.fits' # Open the FITS file on ubuntu
+
+    with fits.open(filename) as hdul:
+        # hdul is a list-like object containing all HDUs (Header Data Units)
+        hdul.info()  # Display information about the HDUs
+        primary_hdu = hdul[0] # Access the primary HDU (header and data)
+        header = primary_hdu.header # The header
+        # print("Header:", header)
+        data = primary_hdu.data # The data (typically image data or a 2D array)
+        # print("Data:", data)
+    
+    ### Plot psfs for visual check
+    saveImsubplots(data, ["PSF 1", "PSF 2", "PSF 3"], filename="psfs", save=True, plot=False)
 
 
-### Plot modulus and phase for quality check
-saveImplot(rho, "Polar modulus", filename="polar_modulus", save=True, plot=False)
-saveImplot(theta, "Polar phase", filename="polar_phase", save=True, plot=False)
-
-
-## Computes the dsp of the object
-p = 2
-k = 1
-rho_0 = 0.01
-
-dspObject = k /(1 + pow(rho/rho_0,p))
-profile = dspObject[0,:1024] # Profile of the radial distance
-freqProfile = freq_vector[:1024] # Profile of the frequency vector
-
-
-### Plot dspObject for quality check
-savePlot(freqProfile, profile, "Profile of the radial distance", filename="profile_radial_distance", save=True, plot=False, logX=True, logY=True)
-saveImplot(dspObject, f"dspObject for rho_0 = {rho_0}", filename=f"dspObject for rho_0 = {rho_0}", save=True, plot=False, logScale=True)
-
-
-## Generate the random variable
-tfObject = np.sqrt(dspObject) * np.random.randn(nPoints, nPoints) # Equivalently : np.sqrt(dspObject) * np.random.randn(nPoints, nPoints)
-
-
-
-## Derive the object
-oObject = np.real(sp.fft.ifft2(tfObject))
-oShapes = oObject.shape
-oObject = oObject[:int(oShapes[0]/2),:int(oShapes[1]/2)] # Select first quadrant, int is equivalent to floor
-oObject = oObject - np.min(oObject) # Remove the offset
-
-
-### Plot oObject for quality check
-saveImplot(oObject, f"Object for rho_0 = {rho_0}", filename=f"Object for rho_0 = {rho_0}", save=True, plot=False, logScale=False)
-
-
-
-## Loading PSF with astropy module, that offers a loading function for Flexible Image Transport Image
-from astropy.io import fits
-
-# 'IO-PTI/data/3psfs_zeroPiSur2EtPi.fits' # Open the FITS file on windows
-filename = './data/3psfs_zeroPiSur2EtPi.fits' # Open the FITS file on ubuntu
-
-with fits.open(filename) as hdul:
-    # hdul is a list-like object containing all HDUs (Header Data Units)
-    hdul.info()  # Display information about the HDUs
-    primary_hdu = hdul[0] # Access the primary HDU (header and data)
-    header = primary_hdu.header # The header
-    # print("Header:", header)
-    data = primary_hdu.data # The data (typically image data or a 2D array)
-    # print("Data:", data)
-
-
-### Plot psfs for visual check
-saveImsubplots(data, ["PSF 1", "PSF 2", "PSF 3"], filename="psfs", save=True, plot=False)
-
-
-
+    return data
 
 
 # Bayesian estimation : Derive alpha from 2 extreme PSF and criterion
-from tqdm.auto import tqdm
-
-psf_1, psf_2, psf_3 = data[0], data[1], data[2]
-N = 20
-xAxis = [round(i /(N + 1),4) for i in range(N + 1)] # Define the alpha axis
-yAxis = np.zeros(N + 1) # Define the Jmap axis
-
-
-## Initialize synthetic image
 from functions import createSyntheticImage
 
-alpha_0 = 0.3
-simImageNoised, sigma = createSyntheticImage(oObject, psf_1, psf_2, alpha_0, verbose=False)
+def createImage(alpha, data, oObject):
+    psf_1, psf_2, psf_3 = data[0], data[1], data[2]
+    N = 20
+    xAxis = [round(i /(N + 1),4) for i in range(N + 1)] # Define the alpha axis
+    yAxis = np.zeros(N + 1) # Define the Jmap axis
+
+    alpha_0 = alpha
+    simImageNoised, sigma = createSyntheticImage(oObject, psf_1, psf_2, alpha_0, verbose=True)
+
+    return simImageNoised, sigma, xAxis, yAxis, psf_1, psf_3, N
 
 
 ## Joint estimation
 from functions import jointMAP2psf
 
-pbar = tqdm(desc="Jmap computation", total=100, unit_scale=True)
+def jointEstimation(oObject, dspObject, psf_1, psf_3, simImageNoised, sigma, xAxis, yAxis, N):    
+    pbar = tqdm(desc="Jmap computation", total=100, unit_scale=True)
 
-for i, alpha in enumerate(xAxis):
-    res = jointMAP2psf(oObject, dspObject, psf_1, psf_3, alpha, simImageNoised, sigma)
-    yAxis[i] = res[0]
-    # print(f"Jmap Value for i = {i}: {yAxis[i]}")
-    pbar.update(round(100 /(N + 1)))
-pbar.close()
+    for i, alpha in enumerate(xAxis):
+        res = jointMAP2psf(oObject, dspObject, psf_1, psf_3, alpha, simImageNoised, sigma)
+        yAxis[i] = res[0]
+        # print(f"Jmap Value for i = {i}: {yAxis[i]}")
+        pbar.update(round(100 /(N + 1)))
+    pbar.close()
 
-### Plot Jmap for quality check
-savePlot(xAxis, yAxis, "Jmap criterion", filename="Jmap_criterion", save=True, plot=True, logX=False, logY=False)
+    ### Plot Jmap for quality check
+    savePlot(xAxis, yAxis, "Jmap criterion", filename="Jmap_criterion", save=True, plot=True, logX=False, logY=False)
 
 
 ## Marginal estimation
-
 from functions import marginalML2psf
 
-pbar = tqdm(desc="Jml computation", total=100, unit_scale=True)
+def marginalEstimation(oObject, dspObject, psf_1, psf_3, simImageNoised, sigma, xAxis, yAxis, N):    
+    pbar = tqdm(desc="Jml computation", total=100, unit_scale=True)
 
-for i, alpha in enumerate(xAxis):
-    res = marginalML2psf(oObject, dspObject, psf_1, psf_3, alpha, simImageNoised, sigma)
-    yAxis[i] = res
-    # print(f"Jml Value for i = {i}: {yAxis[i]}")
-    pbar.update(round(100 /(N + 1)))
-pbar.close()
+    for i, alpha in enumerate(xAxis):
+        res = marginalML2psf(oObject, dspObject, psf_1, psf_3, alpha, simImageNoised, sigma)
+        yAxis[i] = res[0]
+        # print(f"Jml Value for i = {i}: {yAxis[i]}")
+        pbar.update(round(100 /(N + 1)))
+    pbar.close()
 
-### Plot Jmap for quality check
-savePlot(xAxis, yAxis, "Jml criterion", filename="Jml_criterion", save=True, plot=True, logX=False, logY=False)
+    ### Plot Jmap for quality check
+    savePlot(xAxis, yAxis, "Jml criterion", filename="Jml_criterion", save=True, plot=True, logX=False, logY=False)
 
 
+def main():
+    oObject, dspObject = createObject()
+    psfs = openPsfs("ubuntu")
+    simImageNoised, sigma, xAxis, yAxis, psf_1, psf_3, N = createImage(0.3, psfs, oObject)
+    jointEstimation(oObject, dspObject, psf_1, psf_3, simImageNoised, sigma, xAxis, yAxis, N)
+    marginalEstimation(oObject, dspObject, psf_1, psf_3, simImageNoised, sigma, xAxis, yAxis, N)
 
+    
+if __name__ == "__main__":
+    main()
